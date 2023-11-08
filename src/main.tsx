ce4d05fad13041153ed5cd86a5a19ec6c65137f9
Yzma <10090913+Yzma@users.noreply.github.com>
2023-11-08 16:58:34 -0500

    Added react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import './index.css'
 import NavigationContextProvider from './context/NavigationContext.tsx'
 import CallListContextProvider from './context/CallListContext.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <NavigationContextProvider excludeInvalidCalls={false}>
         <CallListContextProvider>
           <App />
