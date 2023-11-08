141c403dbe67d78634049d332b6d81de930638c6
Yzma <10090913+Yzma@users.noreply.github.com>
2023-11-08 16:46:23 -0500

    Cleanup card comments

--- src/components/cards/ErrorCard.tsx
+++ src/components/cards/ErrorCard.tsx
@@ -8,28 +8,6 @@ import {
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-// export default function ErrorCard() {
-//   return (
-//     <Card className="bg-destructive text-white">
-//       {/* <div className="flex h-16 w-full select-none items-center rounded-xl border bg-red-500 p-3 text-sm text-black"> */}
-//       <CardContent>
-//         <div className="flex items-center gap-x-2">
-//           <FontAwesomeIcon icon={faCircleExclamation} className="h-4 w-4" />
-//           <p className="font-semibold text-white">Failed to fetch inbox.</p>
-//         </div>
-//         <Button
-//           variant={'destructive'}
-//           size={'sm'}
-//           className="bg-white text-red-500 hover:bg-gray-200"
-//         >
-//           <FontAwesomeIcon icon={faRotate} className="mr-2 h-4 w-4" />
-//           Retry
-//         </Button>
-//       </CardContent>
-//     </Card>
-//   )
-// }
-
 export const ErrorCard = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
