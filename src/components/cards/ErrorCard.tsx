b115178fb79501e550f2f8a611ec6fd0ac7f46b8
Yzma <10090913+Yzma@users.noreply.github.com>
2023-11-09 00:44:05 -0500

    Update error card to disable and animate when refetching

--- src/components/cards/ErrorCard.tsx
+++ src/components/cards/ErrorCard.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils'
 import { Button } from '@components/ui/button'
 import { Card, CardContent } from '@components/ui/card'
 
@@ -6,28 +7,42 @@ import {
   faRotate,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import useCallList from '@hooks/useCallList'
 import React from 'react'
 
 export const ErrorCard = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
->(({ ...props }, ref) => (
-  <Card className="bg-destructive text-white" ref={ref} {...props}>
-    {/* <div className="flex h-16 w-full select-none items-center rounded-xl border bg-red-500 p-3 text-sm text-black"> */}
-    <CardContent>
-      <div className="flex items-center gap-x-2">
-        <FontAwesomeIcon icon={faCircleExclamation} className="h-4 w-4" />
-        <p className="font-semibold text-white">Failed to fetch inbox.</p>
-      </div>
-      <Button
-        variant={'destructive'}
-        size={'sm'}
-        className="bg-white text-red-500 hover:bg-gray-200"
-      >
-        <FontAwesomeIcon icon={faRotate} className="mr-2 h-4 w-4" />
-        Retry
-      </Button>
-    </CardContent>
-  </Card>
-))
+>(({ ...props }, ref) => {
+  const callList = useCallList()
+  console.log(callList.getAllActivitiesQuery)
+  return (
+    <Card className="bg-destructive text-white" ref={ref} {...props}>
+      {/* <div className="flex h-16 w-full select-none items-center rounded-xl border bg-red-500 p-3 text-sm text-black"> */}
+      <CardContent>
+        <div className="flex items-center gap-x-2">
+          <FontAwesomeIcon icon={faCircleExclamation} className="h-4 w-4" />
+          <p className="font-semibold text-white">Failed to fetch inbox.</p>
+        </div>
+        <Button
+          variant={'destructive'}
+          size={'sm'}
+          className="bg-white text-red-500 hover:bg-gray-200"
+          onClick={() => callList.getAllActivitiesQuery.refetch()}
+          disabled={callList.getAllActivitiesQuery.fetchStatus === 'fetching'}
+        >
+          <FontAwesomeIcon
+            icon={faRotate}
+            className={cn(
+              'mr-2 h-4 w-4',
+              callList.getAllActivitiesQuery.fetchStatus === 'fetching' &&
+                'animate-spin'
+            )}
+          />
+          Retry
+        </Button>
+      </CardContent>
+    </Card>
+  )
+})
 ErrorCard.displayName = 'ErrorCard'
