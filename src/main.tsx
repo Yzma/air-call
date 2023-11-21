2fd4e024e20b0311a1cd4266d82f584391efb941
Yzma <10090913+Yzma@users.noreply.github.com>
2023-11-21 14:25:49 -0500

    Fix build error

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <NavigationContextProvider excludeInvalidCalls={false}>
+      <NavigationContextProvider excludeInvalidActivities={false}>
         <CallListContextProvider>
           <App />
         </CallListContextProvider>
